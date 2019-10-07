581257edf460e663c27bffd7ef7471c02dae259d
lucaschimweg <l.schimweg@gmail.com>
2019-10-07 09:27:05 +0200

    add npm to build script

--- build.sh
+++ build.sh
@@ -3,6 +3,19 @@
 echo "Cleaning up last build"
 rm -rf out
 
+echo "Checking for NPM"
+if ! type "npm" > /dev/null; then
+  echo "NPM not found, installing"
+  if ! type "apt-get" > /dev/null
+  then
+      echo "Apt-get not found, cannot install npm!"
+      exit 1
+  fi
+  apt-get install npm
+else
+  echo "NPM found!"
+fi
+
 echo "Building frontend"
 pushd frontend
   npm install .
