1248c9f960234f0990cca4fe9833d9d7668c4a75
lucaschimweg <l.schimweg@gmail.com>
2019-10-15 22:09:27 +0200

    Improve Build System

--- backend/dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const server = express();
-server.use(express.static("web"));
-server.listen(8080, () => {
-    console.log("Running server...");
-});
-//# sourceMappingURL=main.js.map
