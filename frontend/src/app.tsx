ce2b33904141a8b484ceff3845a4032dc4522348
lucaschimweg <l.schimweg@gmail.com>
2019-10-16 11:05:44 +0200

    some more react tests

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -16,13 +16,11 @@ function updateView() {
 
 updateView();
 
-setTimeout(() => {
-    console.log("update!");
-    const app = new App(123, "WhatsApp")
-    const not = new Notification(1, app, "Message 1", "This is message 1");
-    const not2 = new Notification(2, app, "Message 2", "This is message 2");
-    notifications.push(not);
-    notifications.push(not2);
-    updateView();
-}, 2000);
+console.log("update!");
+const app = new App(123, "WhatsApp")
+const not = new Notification(1, app, "Message 1", "This is message 1");
+const not2 = new Notification(2, app, "Message 2", "This is message 2");
+notifications.push(not);
+notifications.push(not2);
+updateView();
 
