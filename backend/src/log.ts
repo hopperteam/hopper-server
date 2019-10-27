d197c273b8160e80adee7d581f8ce761ef03e8f6
themmj <themmj@users.noreply.github.com>
2019-10-27 16:01:38 +0100

    added proper logger to backend

---
+++ backend/src/log.ts
@@ -0,0 +1,48 @@
+﻿
+enum LogLevel {
+    INFO = "INFO",
+    WARN = "WARN",
+    ERROR = "ERROR"
+}
+
+function persist(entry: string) {
+    // persist log entry in database or log files
+}
+
+function getTimeStamp() {
+    let date = new Date();
+    let year: number = date.getUTCFullYear();
+    let month: number = date.getUTCMonth() + 1;
+    let day: number = date.getUTCDate();
+    let hour: number = date.getUTCHours();
+    let min: number = date.getUTCMinutes();
+    let sec: number = date.getUTCSeconds();
+
+    return `${year}/${(month < 10) ? '0' + month : month}/${(day < 10) ? '0' + day : day}|${(hour < 10) ? '0' + hour : hour}:${(min < 10) ? '0' + min : min}:${(sec < 10) ? '0' + sec : sec}UTC`;
+}
+
+function log(logLevel: LogLevel, module: string, message: string) {
+    let entry = `(${getTimeStamp()}) [${logLevel}] ${module}: ${message}`;
+    persist(entry);
+    console.log(entry);
+}
+
+export default class Log {
+    private module: string;
+
+    constructor(module: string) {
+        this.module = module;
+    }
+
+    public info(message: string) {
+        log(LogLevel.INFO, this.module, message);
+    }
+
+    public warn(message: string) {
+        log(LogLevel.WARN, this.module, message);
+    }
+
+    public error(message: string) {
+        log(LogLevel.ERROR, this.module, message);
+    }
+}
