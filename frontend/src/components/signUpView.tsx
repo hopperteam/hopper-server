cd0e7cf13cb10bd42c1b53115e0cc003c05d5fae
lucaschimweg <l.schimweg@gmail.com>
2019-11-17 22:19:45 +0100

    Fix Placeholder for Repeat Password

--- frontend/src/components/signUpView.tsx
+++ frontend/src/components/signUpView.tsx
@@ -63,8 +63,8 @@ export default class SignUpView extends React.Component<SignUpViewProps, SignUpV
             <form onSubmit={this.onSignUp.bind(this)} target={"#"}>
                 { this.state.error != "" && <span id="error">{this.state.error}</span> }
                 <input name="email" required placeholder="E-Mail" type="email" onChange={(evt) => this.setState({ email: evt.target.value})} value={this.state.email} disabled={this.state.working} /> <br />
-                <input name="password" required placeholder="password" type="password" onChange={(evt) => this.setState({ password: evt.target.value})} value={this.state.password} disabled={this.state.working} /> <br />
-                <input id="repeatedPassword" required name="repeatedPassword" placeholder="password" type="password" onChange={(evt) => this.setState({ repeatedPassword: evt.target.value})} value={this.state.repeatedPassword} disabled={this.state.working} />
+                <input name="password" required placeholder="Password" type="password" onChange={(evt) => this.setState({ password: evt.target.value})} value={this.state.password} disabled={this.state.working} /> <br />
+                <input id="repeatedPassword" required name="repeatedPassword" placeholder="Repeat Password" type="password" onChange={(evt) => this.setState({ repeatedPassword: evt.target.value})} value={this.state.repeatedPassword} disabled={this.state.working} />
                 { this.state.password != this.state.repeatedPassword && <span id="passwordsDoNotMatch">Passwords have to match!</span> } <br />
                 <input name="fName" required placeholder="First Name" type="text" onChange={(evt) => this.setState({ firstName: evt.target.value})} value={this.state.firstName} disabled={this.state.working} /> <br />
                 <input name="lName" required placeholder="Last Name" type="text" onChange={(evt) => this.setState({ lastName: evt.target.value})} value={this.state.lastName} disabled={this.state.working} /> <br />
